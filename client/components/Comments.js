38e4132a2e4b8d2abc640ef50623de12e460939f
tomegz <tomaszbubala@o2.pl>
2017-07-28 22:43:46 +0200

    finish single view

---
+++ client/components/Comments.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+class Comments extends React.Component {
+  renderComment(comment, i) {
+    return (
+      <div className="comment" key={i}>
+        <p>
+          <strong>{comment.user}</strong>
+          {comment.text}
+          <button className="remove-comment">&times;</button>
+        </p>
+      </div>
+    );
+  }
+  render() {
+    return (
+      /*loop through comments and display them if there are any!*/
+      <div className="comments">
+        {this.props.postComments.map(this.renderComment)}
+        <form ref="commentForm" className="comment-form">
+          <input type="text" ref="author" placeholder="author" />
+          <input type="text" ref="comment" placeholder="comment" />
+          <input type="submit" hidden />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Comments;
